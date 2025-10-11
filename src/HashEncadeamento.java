98a04c850f9f554e58fa97c0c14a80596d088a13
Ricardo-LK <rilucaslk@gmail.com>
2025-10-11 17:44:11 -0300

    Hash encadeamento

--- src/HashEncadeamento.java
+++ src/HashEncadeamento.java
@@ -1,10 +1,37 @@
 public class HashEncadeamento extends Hash {
     @Override
-    public EstatisticaHash hash(Registro[] tabelaHash, int tamahoTabelaHash, int[] dados, int tamanhoConjuntoDados) {
-        // Inserindo
-        // Hash ⇒ Busca a posição ⇒ Insere na lista
-        System.out.println("Hash encadeamento não implementado.");
+    public EstatisticaHash hash(Registro[] tabelaHash, int tamanhoTabelaHash, int[] dados, int tamanhoConjuntoDados) {
+        EstatisticaHash estatisticaHash = new EstatisticaHash();
+        estatisticaHash.comecoInsercao = System.nanoTime();
 
-        return null;
+        for (int i = 0; i < tamanhoConjuntoDados; i++) {
+            int dado = dados[i];
+            int hash = funcaoHash(dado, tamanhoTabelaHash);
+
+            if (tabelaHash[hash] == null) {
+                tabelaHash[hash] = new Registro(dado);
+                estatisticaHash.elementosInseridos++;
+
+            } else {
+                Registro novoRegistro = new Registro(dado);
+                novoRegistro.setProximo(tabelaHash[hash]);
+                tabelaHash[hash] =  novoRegistro;
+
+                estatisticaHash.elementosInseridos++;
+                estatisticaHash.colisoes++;
+
+            }
+        }
+
+        estatisticaHash.fimInsercao = System.nanoTime();
+        estatisticaHash.duracaoInsercao = estatisticaHash.fimInsercao - estatisticaHash.comecoInsercao;
+
+        return estatisticaHash;
+    }
+
+    public int funcaoHash(int dado, int modulo) {
+        int h = (dado ^ (dado >>> 16)) % modulo;
+        if (h < 0) return -h;
+        else return h;
     }
 }
